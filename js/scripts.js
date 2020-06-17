613ffd9356259b22f4308cafbd5645cc6fef1bf5
Jacek Jarczok <kson.eu@gmail.com>
2020-06-17 16:00:09 +0200

    Develop buildMenus function

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ const menu_2_ListButtons = document.querySelectorAll('.comp__menu--2 .comp__menu
 const menu_3_ListButtons = document.querySelectorAll('.comp__menu--3 .comp__menu__btn');
 const menus = document.querySelectorAll('.comp__menu');
 const menuLists = document.querySelectorAll('.comp__menu__list');
+const menuList_1 = document.querySelector('.comp__menu__list--1');
+const menuList_2 = document.querySelector('.comp__menu__list--2');
+const menuList_3 = document.querySelector('.comp__menu__list--3');
 const questionMarks = document.querySelectorAll('.comp__feature-title__question-mark');
 
 
@@ -59,7 +62,6 @@ menu_3_ListButtons.forEach(el => {
 })
 
 
-
 // ** Show/hide question mark tooltip **
 
 questionMarks.forEach(el => {
@@ -80,3 +82,41 @@ questionMarks.forEach(el => {
   })
 })
 
+
+/// JSON SCRIPTS
+
+let requestURL = 'https://roborock-compare.k-son.eu/roborock_compare.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+
+request.onload = function() {
+  roborockCompareObject = request.response;
+  buildMenus(roborockCompareObject);
+}
+
+function buildMenus(obj) {
+  const devices = obj.devices;
+
+  for (let j=0; j<menuLists.length; j++) {
+    for (let i=0; i<devices.length; i++) {
+      const listItem = document.createElement('li');
+      listItem.classList.add('comp__menu__list-item');
+  
+      const button = document.createElement('button');
+      button.classList.add('comp__menu__btn');
+      button.textContent = devices[i].name;
+  
+      const span = document.createElement('span');
+      span.classList.add('comp__selected-vacuum');
+      span.classList.add('displayNone');
+  
+      button.appendChild(span);
+      listItem.appendChild(button);
+  
+      menuLists[j].appendChild(listItem);
+    }
+  }
+}
