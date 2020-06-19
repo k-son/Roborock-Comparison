a87c04c2a86093dc611915e480cec8841b776fff
Jacek Jarczok <kson.eu@gmail.com>
2020-06-19 15:02:35 +0200

    Develop functions

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,23 @@ const menuList_2 = document.querySelector('.comp__menu__list--2');
 const menuList_3 = document.querySelector('.comp__menu__list--3');
 const questionMarks = document.querySelectorAll('.comp__feature-title__question-mark');
 
+let devices; // vacuum objects from json file
+
 let menu_1_ListButtons;
 let menu_2_ListButtons;
 let menu_3_ListButtons;
+
+//const volumeFeature = document.getElementById('volume');
+
+const featuresColumn_1 = document.querySelectorAll('comp__feature-exists--1');
+const featuresColumn_2 = document.querySelectorAll('comp__feature-exists--2');
+const featuresColumn_3 = document.querySelectorAll('comp__feature-exists--3');
 
 
 
 //// MAIN SCRIPTS
 
-/// JSON SCRIPTS
+/// RETRIVE JSON
 let requestURL = 'https://roborock-compare.k-son.eu/roborock_compare.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -30,7 +38,7 @@ request.onload = function() {
 }
 
 function buildMenus(obj) {
-  const devices = obj.devices;
+  devices = obj.devices; // take a glance at the json file
 
   // build menus
   for (let j=0; j<menuLists.length; j++) {
@@ -60,7 +68,7 @@ function buildMenus(obj) {
   menu_3_ListButtons = document.querySelectorAll('.comp__menu--3 .comp__menu__btn');
 
   menu_1_ListButtons.forEach(el => {
-    el.addEventListener('click', selectVacuum);
+    el.addEventListener('click', selectVacuum); // maybe selectVacuum(1) and add later 1 to ids like id="volume-1" cos to nie dziala
   })
 
   menu_2_ListButtons.forEach(el => {
@@ -104,7 +112,7 @@ menus.forEach(el => {
 // ** Menu list buttons
 // select vacuum and add approval mark
 
-const selectVacuum = function() {
+function selectVacuum(abc) {
   // remove any approval mark
   const listItems = this.parentElement.parentElement.children;
   Array.from(listItems).forEach(elem => elem.firstElementChild.firstElementChild.classList.add('displayNone'));
@@ -113,8 +121,29 @@ const selectVacuum = function() {
   // hide menu list
   this.parentElement.parentElement.classList.add('displayNone');
   // in menu top button window show name of the selected vacuum 
-  const name = this.textContent;
-  this.parentElement.parentElement.previousElementSibling.firstChild.textContent = name;
+  const vacuumName = this.textContent;
+  this.parentElement.parentElement.previousElementSibling.firstChild.textContent = vacuumName;
+
+  // find selected object
+  const selectedVacuumObject = devices.filter(el => el.name === vacuumName);
+
+  // populate features
+  // clear previous value first 
+  // ***
+
+  /*
+  const volumeValue = document.createElement('span');
+  volumeValue.textContent = selectedVacuumObject[0].volume;
+  volumeValue.classList.add('comp__grid-column-2');
+  volumeFeature.appendChild(volumeValue);
+  */
+
+  // jeżeli funkcja selectVacuum_1 a może uda się selectVacuum(1) to 
+  // wszystim ficzerom z kolumny 1 (comp__feature-exists--1) które jeszcze nie mają klasy 'hideCompareFeature' tę klasę nadajemy
+  // a później usuwamy klasę 'hideCompareFeature' z tych ficzerów które w tym egzemplarzu istnieją (tu wybieramy je po id chyba, id które ma końcówkę --1)
+  // ficzery tekstowe wypełniamy tekstem nowym
+  
+
 }
 
 
